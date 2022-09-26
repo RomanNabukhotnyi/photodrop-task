c9fa7bd58f2696e213ead1d6ae89a54379796acc
RomanNabukhotnyi <97584054+RomanNabukhotnyi@users.noreply.github.com>
2022-09-26 19:31:36 +0300

    added watermark overlay on photo

--- photodrop-client/src/functions/getAlbum/handler.ts
+++ photodrop-client/src/functions/getAlbum/handler.ts
@@ -1,7 +1,16 @@
 import createError from 'http-errors';
+import * as AWS from 'aws-sdk';
 
 import { middyfy } from '../../libs/lambda';
 import { ClientPhotos } from '../../db/entity/clientPhotos';
+
+const S3 = new AWS.S3({
+    signatureVersion: 'v4',
+    credentials: new AWS.Credentials({
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    }),
+});
 
 const getAlbum = async (event) => {
     const number: string = event.requestContext.authorizer.principalId;
@@ -19,10 +28,18 @@ const getAlbum = async (event) => {
         name,
         location: Items[0].location,
         date: Items[0].date,
-        photos: Items.map(item => ({
-            url: item.url,
-            watermark: item.watermark,
-        })),
+        photos: Items.map(item => {
+
+            const url = item.watermark ? S3.getSignedUrl('getObject', {
+                Bucket: process.env.LAMBDA_ACCESS_POINT_ARN,
+                Key: item.url.replace(`https://${process.env.PHOTOGRAPHER_BUCKET_NAME}.s3.amazonaws.com/`, ''),
+                Expires: 86400,
+            }) : item.url;
+            return {
+                url,
+                watermark: item.watermark,
+            };
+        }),
     };
 };
 
